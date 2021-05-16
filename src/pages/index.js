23369046b8f7d322d4184ba4b7e0cfa7321c3d30
Andrew Heller <aheller9@gmail.com>
2021-05-16 11:16:11 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ function IndexPage(props) {
       bgImageOpacity={1}
       title="Notion API Test"
       subtitle="Demo code to read and write Data to your Notion workspace from a Next.js Application."
-      image="https://uploads.divjoy.com/undraw-japan_ubgk.svg"
+      image="https://raw.githubusercontent.com/andheller/notion-api-boilerplate/f0bab35b9d1ce387458847c4c9b14bb43ed9262e/public/undraw_Code_thinking_re_gka2.svg"
       buttonText="View on GitHub"
       buttonColor="primary"
       buttonPath="https://github.com/andheller/notion-api-boilerplate"
